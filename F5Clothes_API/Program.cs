245b4b1f7e66ac3326df7ee52c72d6dbb7813860
thành hà <hathanhks01@gmail.com>
2024-09-11 16:43:49 +0700

    authentication

--- F5Clothes_API/Program.cs
+++ F5Clothes_API/Program.cs
@@ -1,15 +1,62 @@
+﻿using F5Clothes_DAL.IReponsitories;
+using F5Clothes_DAL.Models.system;
+using F5Clothes_DAL.Models;
+using F5Clothes_DAL.Reponsitories;
+using F5Clothes_Services.IServices;
+using F5Clothes_Services.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Cấu hình dịch vụ DbContext với chuỗi kết nối từ appsettings.json
+builder.Services.AddDbContext<DbduAnTnContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Cấu hình dịch vụ JWTSettings từ appsettings.json
+builder.Services.Configure<JWTSetting>(builder.Configuration.GetSection("Jwt"));
+
+// Đảm bảo bạn đang sử dụng cùng tên cấu hình
+builder.Services.AddSingleton(resolver =>
+    resolver.GetRequiredService<IOptions<JWTSetting>>().Value);
+
+// Cấu hình dịch vụ Authentication với JWT Bearer
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JWTSetting>();
+    if (jwtSettings == null)
+    {
+        throw new Exception("JWTSettings configuration section is missing or invalid.");
+    }
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings.Issuer,
+        ValidAudience = jwtSettings.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+    };
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IAuthenticationRepo, AuthenticationRepo>();
+builder.Services.AddScoped<IAuthenticationServices, AuthenticationServices>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,7 +64,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- F5Clothes_Services/IServices/IAuthenticationServices.cs
+++ F5Clothes_Services/IServices/IAuthenticationServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using F5Clothes_DAL.Models.system;
+using F5Clothes_DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,7 @@ namespace F5Clothes_Services.IServices
 {
     public interface IAuthenticationServices
     {
+        public Task<Object> Login(string username, string password);
+        public Task<(KhachHang, String)> Register(Customer customer);
     }
 }
