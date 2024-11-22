77e5ed6d89d581b9ad99de3719891daa1ecb9b33
Tungdv <doantung.dev@gmail.com>
2024-11-22 15:46:52 +0700

    tungdv update

--- F5Clothes_DAL/IReponsitories/ISanPhamRepo.cs
+++ F5Clothes_DAL/IReponsitories/ISanPhamRepo.cs
@@ -12,7 +12,7 @@ namespace F5Clothes_DAL.IReponsitories
 {
     public interface ISanPhamRepo
     {
-        Task<List<SanPham>> GetAllSanPham();
+        Task<IEnumerable<object>> GetAllSanPham();
         Task<SanPham> GetByIdSanPham(Guid id);
         Task<SanPham> AddSanPham(SanPhamDtos sanPhamDto);
         Task<SanPham> UpdateSanPham(SanPhamDtos sanPhamDto);
