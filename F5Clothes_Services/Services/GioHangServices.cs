23d37dca6bcb7c39ad1c40e8565ee103b1976348
DuyAn <duyanhexat@gmail.com>
2024-12-15 23:23:56 +0700

    duyan  update 15/12

--- F5Clothes_Services/Services/GioHangServices.cs
+++ F5Clothes_Services/Services/GioHangServices.cs
@@ -302,16 +302,13 @@ namespace F5Clothes_Services.Services
                 throw new Exception($"Không đủ số lượng sản phẩm {product.TenSp} trong kho.");
             }
 
-            // Map DTO fields to the existing cart item
+           
             existingCartItem.SoLuong = updateDto.SoLuong;
-            // Set other fields if needed
-            // Example: existingCartItem.IdSpct = updateDto.IdSpct;
-
-            // Update the cart item in the repository
+           
             await _gioHangRepo.UpdateGioHangAsync(existingCartItem);
         }
 
-        // Delete a cart item
+        
         public async Task DeleteGioHangAsync(Guid id)
         {
             var existingCartItem = await _gioHangRepo.GetGioHangByIdAsync(id);
